9893b1974710b372c81ba9e5d3d7ab6e512c6992
Michael <mizitoh@hotmail.com>
2023-11-04 11:08:45 -0300

    configuração eureka

--- hr-worker/src/main/java/com/michael/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/michael/hrworker/HrWorkerApplication.java
@@ -2,7 +2,9 @@ package com.michael.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
 
